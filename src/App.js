e12fe1dbf146603859e874fc5440b8576659544a
suatcg <suat-can_24@hotmail.com>
2022-08-30 19:18:41 +0300

    FETCH DATA: onClick Button

---
+++ src/App.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import { CircleLoader } from "react-spinners";
+import "./App.css";
+
+const CSSProperties = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "purple",
+};
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function fetchMoviesHandler() {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }
+
+  let content = <p>Found no movies.</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (error) {
+    content = <p>{error}</p>;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        {/* {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && <p>Found no movies</p>}
+        {!isLoading && error && <p>{error}</p>}
+        {isLoading && <p>loading...</p>} */}
+        {content}
+        <CircleLoader loading={isLoading} cssOverride={CSSProperties} size={150} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
